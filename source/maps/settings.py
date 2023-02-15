fc37c1258dbb18a6243bd7e5c6e25bb20685aeb7
ghost <kirill.han011@gmail.com>
2023-02-16 01:09:15 +0300

    Добавлен Dockerfile

--- source/maps/settings.py
+++ source/maps/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'webapp')
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8000",
+    "91.106.207.103"
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
